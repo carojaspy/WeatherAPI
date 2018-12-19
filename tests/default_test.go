a171edf2a96d2ea220c29142795f4bf11c37fb66
carlosalberto.rojas <carlosalberto.rojas@globant.com>
2018-12-18 19:09:56 -0600

    Fixed absolute path in default_test.go Adding tests cases and DB Connection

--- tests/default_test.go
+++ tests/default_test.go
@@ -1,7 +1,8 @@
 package test
 
 import (
-	_ "WeatherAPI/routers"
+	"encoding/json"
+	"io/ioutil"
 	"log"
 	"net/http"
 	"net/http/httptest"
@@ -9,35 +10,40 @@ import (
 	"runtime"
 	"testing"
 
+	_ "github.com/carojaspy/WeatherAPI/routers"
+	_ "github.com/go-sql-driver/mysql"
+
 	"github.com/astaxie/beego"
+	"github.com/astaxie/beego/orm"
 	. "github.com/smartystreets/goconvey/convey"
 )
 
 func init() {
 	_, file, _, _ := runtime.Caller(1)
-	log.Print(file)
-	// apppath, _ := filepath.Abs(filepath.Dir(filepath.Join(file, ".."+string(filepath.Separator))))
-	apppath := "/mnt/c/Users/carlosalberto.rojas/Desktop/go/src/WeatherAPI/"
-	// apppath := "\\mnt\\c\\Users\\carlosalberto.rojas\\Desktop\\go\\src\\WeatherAPI\\"
-
-	log.Print("apppath: ")
-	log.Print(apppath)
-	log.Print(string(filepath.Separator))
+	apppath, _ := filepath.Abs(filepath.Dir(filepath.Join(file, "../.."+string(filepath.Separator))))
 
 	beego.TestBeegoInit(apppath)
+
+	// Connect to Database
+	orm.RegisterDataBase("default", "mysql", "root:@/weatherapi?charset=utf8")
+	o := orm.NewOrm()
+	orm.Debug = true
+	o.Using("default") // Using default, you can use other database
+
 }
 
-// TestGet is a sample to run an endpoint test
-func TestGet(t *testing.T) {
-	r, _ := http.NewRequest("GET", "/v1/object", nil)
+// TestEmptyGet Request Without params, should be fail
+func TestEmptyGet(t *testing.T) {
+
+	r, _ := http.NewRequest("GET", "/v1/weather", nil)
 	w := httptest.NewRecorder()
 	beego.BeeApp.Handlers.ServeHTTP(w, r)
+	beego.Trace("testing", "TestEmptyGet")
+	log.Println(w.Code)
 
-	beego.Trace("testing", "TestGet", "Code[%d]\n%s", w.Code, w.Body.String())
-
-	Convey("Subject: Test Station Endpoint\n", t, func() {
-		Convey("Status Code Should Be 200", func() {
-			So(w.Code, ShouldEqual, 200)
+	Convey("Subject: Test Get Weather Request\n", t, func() {
+		Convey("Status Code Should Be 404 for incomplete params", func() {
+			So(w.Code, ShouldEqual, 404)
 		})
 		Convey("The Result Should Not Be Empty", func() {
 			So(w.Body.Len(), ShouldBeGreaterThan, 0)
@@ -45,17 +51,61 @@ func TestGet(t *testing.T) {
 	})
 }
 
-func TestWeather(t *testing.T) {
+// TestEmptyGet Request Without params, should be fail
+func TestGet(t *testing.T) {
 
-	Convey("Given a HTTP request for /weather Without GET params", t, func() {
-		req, _ := http.NewRequest("GET", "/weather", nil)
-		resp := httptest.NewRecorder()
-		beego.BeeApp.Handlers.ServeHTTP(resp, req)
+	Convey("Subject: Test Get Weather Request with City and Country params\n", t, func() {
+		r, _ := http.NewRequest("GET", "/v1/weather?city=Peru&country=us", nil)
+		w := httptest.NewRecorder()
+		beego.BeeApp.Handlers.ServeHTTP(w, r)
+		beego.Trace("testing", "TestGet")
 
-		Convey("When the request is handled by the Router", func() {
-			Convey("Then the response should be a 404", func() {
-				So(resp.Code, ShouldEqual, 404)
-			})
+		Convey("Status Code Should Be 200", func() {
+			So(w.Code, ShouldEqual, 200)
+		})
+
+		Convey("The Result Should be a JSON response", func() {
+			body, err := ioutil.ReadAll(w.Body)
+			// Building response
+			var wjson interface{}
+			err = json.Unmarshal(body, &wjson)
+			So(err, ShouldBeNil)
 		})
 	})
+
+	Convey("Subject: Test Get Weather Request with wrong City(Right) and Country(Wrong) params\n", t, func() {
+		r, _ := http.NewRequest("GET", "/v1/weather?city=Peru&country=test", nil)
+		w := httptest.NewRecorder()
+		beego.BeeApp.Handlers.ServeHTTP(w, r)
+		beego.Trace("testing", "TestGet")
+
+		Convey("Status Code Should Be 200", func() {
+			So(w.Code, ShouldEqual, 200)
+		})
+
+		Convey("The Result Should be a JSON response", func() {
+			body, err := ioutil.ReadAll(w.Body)
+			// Building response
+			var wjson interface{}
+			err = json.Unmarshal(body, &wjson)
+			So(err, ShouldBeNil)
+		})
+	})
+
+	Convey("Subject: Test Get Weather Request with wrong City(Wrong) and Country(Right) params\n", t, func() {
+		r, _ := http.NewRequest("GET", "/v1/weather?city=test&country=us", nil)
+		w := httptest.NewRecorder()
+		beego.BeeApp.Handlers.ServeHTTP(w, r)
+		beego.Trace("testing", "TestGet")
+
+		Convey("Status Code Should Be 404", func() {
+			So(w.Code, ShouldEqual, 404)
+		})
+
+		Convey("The Result ShouldContainSubstring 'not found' text response", func() {
+			body, _ := ioutil.ReadAll(w.Body)
+			So(string(body), ShouldContainSubstring, "Not found")
+		})
+	})
+
 }
